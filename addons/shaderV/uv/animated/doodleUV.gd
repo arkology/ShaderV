215f15726ca7ebd322dab9aa521b104a3d80c27e
arkology <43543909+arkology@users.noreply.github.com>
2019-11-03 11:25:56 +0300

    Now doodleUV works as expected

--- addons/shaderV/uv/animated/doodleUV.gd
+++ addons/shaderV/uv/animated/doodleUV.gd
@@ -65,25 +65,28 @@ func _get_output_port_type(port):
 	return VisualShaderNode.PORT_TYPE_VECTOR
 
 func _get_global_code(mode):
-	return """float hash2D00dle(vec2 p) { return fract(1e4 * sin(17.0 * p.x + p.y * 0.1) * (0.1 + abs(sin(p.y * 13.0 + p.x)))); }
+	return """
+float hash2D00dle(vec2 _p_hash_d00dle) {
+	return fract(1e4 * sin(17.0 * _p_hash_d00dle.x + _p_hash_d00dle.y * 0.1) * (0.1 + abs(sin(_p_hash_d00dle.y * 13.0 + _p_hash_d00dle.x))));
+}
 
 float noiseD00dle(vec2 _seed_noise_d00dle){
 	vec2 i = floor(_seed_noise_d00dle);
 	vec2 f = fract(_seed_noise_d00dle);
-	float a = hash2D00dle(i);
-	float b = hash2D00dle(i + vec2(1.0, 0.0));
-	float c = hash2D00dle(i + vec2(0.0, 1.0));
-	float d = hash2D00dle(i + vec2(1.0, 1.0));
+	float _a = hash2D00dle(i);
+	float _b = hash2D00dle(i + vec2(1.0, 0.0));
+	float _c = hash2D00dle(i + vec2(0.0, 1.0));
+	float _d = hash2D00dle(i + vec2(1.0, 1.0));
 	vec2 u = f * f * (3.0 - 2.0 * f);
-	return mix(a, b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
+	return mix(_a, _b, u.x) + (_c - _a) * u.y * (1.0 - u.x) + (_d - _b) * u.x * u.y;
 }
 
 vec2 d00dleUVFunc(vec2 _uv_d00dle, float _max_offset_d00dle, float _time_d00dle, float _frame_time_d00dle, int _frame_count_d00dle, float _scale_d00dle){
 	float timeValueD00dle = mod(floor(_time_d00dle / _frame_time_d00dle), float(_frame_count_d00dle)) + 1.0;
-	return vec2(noiseD00dle((_uv_d00dle + timeValueD00dle) * _scale_d00dle) * 2.0 - 1.0) * _max_offset_d00dle;
+	return _uv_d00dle + vec2(noiseD00dle((_uv_d00dle + timeValueD00dle) * _scale_d00dle) * 2.0 - 1.0) * _max_offset_d00dle;
 }
 """
 
 func _get_code(input_vars, output_vars, mode, type):
-	return "%s.xy += d00dleUVFunc(%s.xy, %s, %s, %s, int(%s), %s);" % [
+	return "%s.xy = d00dleUVFunc(%s.xy, %s, %s, %s, int(%s), %s);" % [
 	output_vars[0], input_vars[0], input_vars[4], input_vars[5], input_vars[1], input_vars[2], input_vars[3]]
