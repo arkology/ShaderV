b356b489666f7f97ef021106500817cf356a6c01
arkology <43543909+arkology@users.noreply.github.com>
2019-11-17 17:43:17 +0300

    New nodes: turn color to GB and CGA palettes

---
+++ addons/shaderV/rgba/turnCGA4Palette.gd
@@ -0,0 +1,73 @@
+tool
+extends VisualShaderNodeCustom
+class_name VisualShaderNodeRGBAturnCGA4Palette
+
+func _get_name() -> String:
+	return "TurnCGA4Palette"
+
+func _get_category() -> String:
+	return "RGBA"
+
+#func _get_subcategory():
+#	return ""
+
+func _get_description() -> String:
+	return "Swaps color to CGA 4-color palette"
+
+func _get_return_icon_type() -> int:
+	return VisualShaderNode.PORT_TYPE_VECTOR
+
+func _get_input_port_count() -> int:
+	return 2
+
+func _get_input_port_name(port: int):
+	match port:
+		0:
+			return "color"
+		1:
+			return "threshold"
+
+func _get_input_port_type(port: int):
+	set_input_port_default_value(1, 1.5)
+	match port:
+		0:
+			return VisualShaderNode.PORT_TYPE_VECTOR
+		0:
+			return VisualShaderNode.PORT_TYPE_SCALAR
+
+func _get_output_port_count() -> int:
+	return 1
+
+func _get_output_port_name(port: int) -> String:
+	return "col"
+
+func _get_output_port_type(port: int) -> int:
+	return VisualShaderNode.PORT_TYPE_VECTOR
+
+func _get_global_code(mode: int) -> String:
+	return """
+vec3 cg4Pa1etteFunc(vec3 _c0l_cga, float _g4mm4_cga){
+	_c0l_cga = pow(_c0l_cga, vec3(_g4mm4_cga));
+	float _gr4y_c0l_cga4 = 0.21 * _c0l_cga.r + 0.71 * _c0l_cga.g + 0.07 * _c0l_cga.b;
+	vec3 _re5_c0l_cga4 = vec3(0.0);
+	
+	vec3 _CGAPa1[4] = vec3[4] ( vec3(0.0,  0.0,  0.0),
+								vec3(1.0,  0.33, 1.0),
+								vec3(0.33, 1.0,  1.0),
+								vec3(1.0,  1.0,  1.0));
+	
+	if      (_gr4y_c0l_cga4 <= 1.0/4.0)
+		_re5_c0l_cga4 = _CGAPa1[0];
+	else if (_gr4y_c0l_cga4 <= 2.0/4.0)
+		_re5_c0l_cga4 = _CGAPa1[1];
+	else if (_gr4y_c0l_cga4 <= 3.0/4.0)
+		_re5_c0l_cga4 = _CGAPa1[2];
+	else if (_gr4y_c0l_cga4 <= 4.0/4.0)
+		_re5_c0l_cga4 = _CGAPa1[3];
+	
+	return _re5_c0l_cga4;
+}
+"""
+
+func _get_code(input_vars: Array, output_vars: Array, mode: int, type: int) -> String:
+	return "%s = cg4Pa1etteFunc(%s, %s);" % [output_vars[0], input_vars[0], input_vars[1]]
---
+++ addons/shaderV/rgba/turnGBPalette.gd
@@ -0,0 +1,73 @@
+tool
+extends VisualShaderNodeCustom
+class_name VisualShaderNodeRGBAturnGameBoyPalette
+
+func _get_name() -> String:
+	return "TurnGameBoyPalette"
+
+func _get_category() -> String:
+	return "RGBA"
+
+#func _get_subcategory():
+#	return ""
+
+func _get_description() -> String:
+	return "Swaps color to GameBoy palette"
+
+func _get_return_icon_type() -> int:
+	return VisualShaderNode.PORT_TYPE_VECTOR
+
+func _get_input_port_count() -> int:
+	return 2
+
+func _get_input_port_name(port: int):
+	match port:
+		0:
+			return "color"
+		1:
+			return "threshold"
+
+func _get_input_port_type(port: int):
+	set_input_port_default_value(1, 1.5)
+	match port:
+		0:
+			return VisualShaderNode.PORT_TYPE_VECTOR
+		0:
+			return VisualShaderNode.PORT_TYPE_SCALAR
+
+func _get_output_port_count() -> int:
+	return 1
+
+func _get_output_port_name(port: int) -> String:
+	return "col"
+
+func _get_output_port_type(port: int) -> int:
+	return VisualShaderNode.PORT_TYPE_VECTOR
+
+func _get_global_code(mode: int) -> String:
+	return """
+vec3 g4meb0yPa1etteFunc(vec3 _c0l_g4b0, float _g4mm4_g4b0){
+	_c0l_g4b0 = pow(_c0l_g4b0, vec3(_g4mm4_g4b0));
+	float _gr4y_c0l_g4b0 = 0.21 * _c0l_g4b0.r + 0.71 * _c0l_g4b0.g + 0.07 * _c0l_g4b0.b;
+	vec3 _re5_c0l_g4b0 = vec3(0.0);
+	
+	vec3 _Gameb0yPa1[4] = vec3[4] ( vec3(0.612, 0.725, 0.086),
+									vec3(0.549, 0.667, 0.078),
+									vec3(0.188, 0.392, 0.188),
+									vec3(0.063, 0.247, 0.063));
+	
+	if      (_gr4y_c0l_g4b0 <= 1.0/4.0)
+		_re5_c0l_g4b0 = _Gameb0yPa1[3];
+	else if (_gr4y_c0l_g4b0 <= 2.0/4.0)
+		_re5_c0l_g4b0 = _Gameb0yPa1[2];
+	else if (_gr4y_c0l_g4b0 <= 3.0/4.0)
+		_re5_c0l_g4b0 = _Gameb0yPa1[1];
+	else if (_gr4y_c0l_g4b0 <= 4.0/4.0)
+		_re5_c0l_g4b0 = _Gameb0yPa1[0];
+	
+	return _re5_c0l_g4b0;
+}
+"""
+
+func _get_code(input_vars: Array, output_vars: Array, mode: int, type: int) -> String:
+	return "%s = g4meb0yPa1etteFunc(%s, %s);" % [output_vars[0], input_vars[0], input_vars[1]]
